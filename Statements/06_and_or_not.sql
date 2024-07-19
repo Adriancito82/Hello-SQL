97f314ec454086a3e9624e270d15c06fd8f2e410
adrian <profilte82@gmail.com>
2024-07-19 22:49:32 +0100

    LIMIT

---
+++ Statements/06_and_or_not.sql
@@ -0,0 +1,5 @@
+SELECT * FROM users WHERE NOT email = 'ulises@gmail.com';
+
+SELECT * FROM users WHERE NOT email = 'ulises@gmail.com' AND age = 15;
+
+SELECT * FROM users WHERE NOT email = 'ulises@gmail.com' OR age = 15;
---
+++ Statements/07_limit.sql
@@ -0,0 +1,3 @@
+SELECT * FROM users LIMIT 3;
+
+SELECT * FROM users WHERE NOT email = 'ulises@gmail.com' OR age = 15 LIMIT 2;
