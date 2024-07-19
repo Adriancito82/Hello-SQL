e398c61567aa44422b5538c27c27b37a338ca202
adrian <profilte82@gmail.com>
2024-07-19 22:37:34 +0100

    LIKE actulizado

--- Statements/05_like.sql
+++ Statements/05_like.sql
@@ -3,3 +3,5 @@ SELECT * FROM users WHERE email LIKE '%gmail.com';
 SELECT * FROM users WHERE email LIKE 'adrian%';
 
 SELECT * FROM users WHERE email LIKE '%@%';
+
+SELECT * FROM users WHERE name LIKE 'A__i__';
