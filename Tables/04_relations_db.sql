369768e27f0ba3aba04909a2a9b4334c21083730
adrian <profilte82@gmail.com>
2024-07-27 11:38:33 +0100

    Insert datos relacionados

--- Tables/04_relations_db.sql
+++ Tables/04_relations_db.sql
@@ -39,9 +39,11 @@ CREATE TABLE users_languages(
 );
 
 -- INSERT
+-- 1:1
 INSERT INTO dni (dni_number, user_id) VALUES (33333333, 3);
 INSERT INTO dni (dni_number, user_id) VALUES (44444444, 4);
 
+-- 1:N
 INSERT INTO companies (name) VALUES ('Apple');
 INSERT INTO companies (name) VALUES ('Google');
 INSERT INTO companies (name) VALUES ('Microsoft');
@@ -51,3 +53,21 @@ UPDATE users SET company_id = 1 WHERE user_id = 3;
 UPDATE users SET company_id = 3 WHERE user_id = 2;
 UPDATE users SET company_id = 3 WHERE user_id = 5;
 UPDATE users SET company_id = 2 WHERE user_id = 10;
+
+-- N:M
+INSERT INTO languages (name) VALUE ('Swift');
+INSERT INTO languages (name) VALUE ('JavaScript');
+INSERT INTO languages (name) VALUE ('React');
+INSERT INTO languages (name) VALUE ('PHP');
+INSERT INTO languages (name) VALUE ('Python');
+
+INSERT INTO users_languages (user_id, language_id) VALUES (1, 1);
+INSERT INTO users_languages (user_id, language_id) VALUES (1, 3);
+INSERT INTO users_languages (user_id, language_id) VALUES (1, 4);
+INSERT INTO users_languages (user_id, language_id) VALUES (2, 4);
+INSERT INTO users_languages (user_id, language_id) VALUES (2, 2);
+INSERT INTO users_languages (user_id, language_id) VALUES (2, 1);
+INSERT INTO users_languages (user_id, language_id) VALUES (3, 3);
+INSERT INTO users_languages (user_id, language_id) VALUES (3, 4);
+INSERT INTO users_languages (user_id, language_id) VALUES (4, 4);
+INSERT INTO users_languages (user_id, language_id) VALUES (4, 5);
